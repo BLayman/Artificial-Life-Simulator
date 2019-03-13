cff9b89c2626bd13575b9be5c24f5d2f3bac6501
LaymanB <leasure57@gmail.com>
2019-03-12 21:14:47 -0600

    Added an script so that the user can press ESC to quit.

---
+++ Assets/Scripts/EscToExit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscToExit : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
