f932aa8fe6ef40138b3b74b918bb174bc8df12ac
LaymanB <leasure57@gmail.com>
2019-05-13 17:46:18 -0600

    Added main menu alternative.

---
+++ Assets/Scripts/StartWithoutMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWithoutMenu : MonoBehaviour
+{
+    public GameObject managerObj;
+    public GameObject cameraObj;
+    public GameObject dropDown;
+    public int demoIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        managerObj.GetComponent<ThreadManager>().menuStart(demoIndex);
+        managerObj.GetComponent<SimRunnerTest>().menuStart();
+        cameraObj.GetComponent<CameraPositioner>().menuStart();
+        dropDown.GetComponent<ResourceSelectorDD>().menuStart();
+
+    }
+
+
+}
